3c4477faf23b9d981f16895619b4c58930625db4
gerardogarnica <gerardo.garnica@gmail.com>
2024-01-25 15:51:03 -0500

    Aurora.BizSuite.Settings.Infrastructure: add setting context class

---
+++ src/Modules/Settings/Aurora.BizSuite.Settings.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Aurora.BizSuite.Settings.Domain.Options;
+global using Aurora.BizSuite.Settings.Infrastructure.Repositories;
+global using Aurora.Framework;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
---
+++ src/Modules/Settings/Aurora.BizSuite.Settings.Infrastructure/SettingsContext.cs
@@ -0,0 +1,57 @@
+﻿namespace Aurora.BizSuite.Settings.Infrastructure;
+
+public class SettingsContext : DbContext, IUnitOfWork
+{
+    #region DbSet properties
+
+    public DbSet<Option> Options { get; set; } = null!;
+    public DbSet<OptionItem> OptionItems { get; set; } = null!;
+
+    #endregion
+
+    internal const string DEFAULT_SCHEMA = "SET";
+
+    private readonly IPublisher _publisher;
+
+    public SettingsContext(
+        DbContextOptions<SettingsContext> options,
+        IPublisher publisher)
+        : base(options)
+    {
+        _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SettingsContext).Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEvents = ChangeTracker
+            .Entries<IBaseEntity>()
+            .Select(x => x.Entity)
+            .Where(x => x.DomainEvents.Count != 0)
+            .SelectMany(x =>
+            {
+                var domainEvents = x.DomainEvents;
+                x.ClearDomainEvents();
+                return domainEvents;
+            })
+            .ToList();
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        foreach (var domainEvent in domainEvents)
+            await _publisher.Publish(domainEvent, cancellationToken);
+
+        return result;
+    }
+
+    public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
