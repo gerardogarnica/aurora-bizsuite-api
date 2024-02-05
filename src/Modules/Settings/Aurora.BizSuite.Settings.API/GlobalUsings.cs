18df2cf8972db77759e159f1d539d5e8d0c8470c
gerardogarnica <gerardo.garnica@gmail.com>
2024-02-05 11:47:40 -0500

    Add Aurora.BizSuite.Settings.API project

---
+++ src/Modules/Settings/Aurora.BizSuite.Settings.API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Aurora.Framework;
+global using Aurora.Framework.Api;
+global using MediatR;
+global using Microsoft.AspNetCore.Http.HttpResults;
---
+++ src/Modules/Settings/Aurora.BizSuite.Settings.API/Program.cs
@@ -0,0 +1,26 @@
+using Aurora.BizSuite.Settings.Application;
+using Aurora.BizSuite.Settings.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.AddEndpoints();
+
+app.Run();
