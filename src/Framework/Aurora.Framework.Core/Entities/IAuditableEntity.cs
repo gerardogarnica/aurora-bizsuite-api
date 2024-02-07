07131f65c87737f861fd27b652c9d21f5b661848
gerardogarnica <gerardo.garnica@gmail.com>
2024-02-06 21:42:05 -0500

    Aurora.Framework.Core: change auditable entity to an interface

---
+++ src/Framework/Aurora.Framework.Core/Entities/IAuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Aurora.Framework;
+
+public interface IAuditableEntity
+{
+    public string? CreatedBy { get; }
+    public DateTime CreatedAt { get; }
+    public string? UpdatedBy { get; }
+    public DateTime? UpdatedAt { get; }
+}
