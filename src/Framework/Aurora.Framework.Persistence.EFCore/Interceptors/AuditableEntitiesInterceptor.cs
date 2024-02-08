dfb58a916307d977e9a497b7376cdccbe206610e
gerardogarnica <gerardo.garnica@gmail.com>
2024-02-08 15:10:35 -0500

    Add new Aurora.Framework.Persistence.EFCore class library

---
+++ src/Framework/Aurora.Framework.Persistence.EFCore/Interceptors/AuditableEntitiesInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Aurora.Framework.Persistence.EFCore;
+
+public sealed class AuditableEntitiesInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is not null)
+            UpdateAuditableEntities(eventData.Context);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void UpdateAuditableEntities(DbContext context)
+    {
+        var now = DateTime.UtcNow;
+        var user = "System";
+        var auditableEntities = context
+            .ChangeTracker
+            .Entries<IAuditableEntity>();
+
+        foreach (var entry in auditableEntities)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.SetCreated(user, now);
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.SetUpdated(user, now);
+            }
+        }
+    }
+}
---
+++ src/Framework/Aurora.Framework.Persistence.EFCore/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Framework.Persistence.EFCore;
+
+public abstract class BaseRepository<TEntity, TId>
+    where TEntity : AggregateRoot<TId>
+    where TId : class
+{
+    private readonly DbContext _context;
+
+    protected BaseRepository(DbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public virtual async Task<TEntity?> GetByIdAsync(TId id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task<TEntity> InsertAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+
+        return entity;
+    }
+
+    public void Update(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+    }
+
+    protected async Task<PagedResult<T>> ToPagedResultAsync<T>(
+        IQueryable<T> query,
+        PagedViewRequest viewRequest)
+        where T : class
+    {
+        var totalItems = await query.CountAsync();
+
+        var items = await query
+            .Skip((viewRequest.PageIndex - 1) * viewRequest.PageSize)
+            .Take(viewRequest.PageSize)
+            .ToListAsync();
+
+        var currentPage = totalItems > 0 ? viewRequest.PageIndex + 1 : 0;
+
+        var totalPages = (int)Math.Ceiling(totalItems / (double)viewRequest.PageSize);
+
+        if (totalPages < currentPage)
+            throw new ArgumentOutOfRangeException("The current page cannot be greater than the total pages.");
+
+        return new PagedResult<T>(
+            items,
+            totalItems,
+            currentPage,
+            totalPages);
+    }
+}
