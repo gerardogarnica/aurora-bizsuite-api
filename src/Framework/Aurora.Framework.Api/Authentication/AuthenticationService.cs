b08f5a3e6795e571f5fcf969e687a6b845c06f9c
gerardogarnica <gerardo.garnica@gmail.com>
2024-02-22 15:30:58 -0500

    Aurora.Framework.Api: add authentication service configuration

---
+++ src/Framework/Aurora.Framework.Api/Authentication/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using Aurora.Framework.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Framework.Api;
+
+public static class AuthenticationService
+{
+    public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection("Jwt"));
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer();
+
+        services.AddScoped<IJwtProvider, JwtProvider>();
+
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+        return services;
+    }
+}
---
+++ src/Framework/Aurora.Framework.Api/Authentication/JwtBearerOptionsSetup.cs
@@ -0,0 +1,32 @@
+﻿using Aurora.Framework.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Aurora.Framework.Api;
+
+public class JwtBearerOptionsSetup(IOptions<JwtOptions> jwtOptions) : IConfigureNamedOptions<JwtBearerOptions>
+{
+    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
+    public void Configure(string? name, JwtBearerOptions options)
+    {
+        options.SaveToken = true;
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _jwtOptions.Issuer,
+            ValidAudience = _jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey))
+        };
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        Configure("Bearer", options);
+    }
+}
