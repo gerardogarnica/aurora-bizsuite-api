e99eb216f995554fb862a37eeaca36dd89bff5ee
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-07 11:52:38 -0500

    Aurora.Framework.Api: add IExceptionHandler instead of middleware

---
+++ src/Framework/Aurora.Framework.Api/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Aurora.Framework.Api;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "Exception ocurred: {Message}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Type = "Server Error",
+            Title = "Internal Server Error",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = "An error occurred while processing your request."
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/json";
+
+        var json = JsonSerializer.Serialize(problemDetails);
+
+        await httpContext.Response.WriteAsync(json, cancellationToken: cancellationToken);
+
+        return true;
+    }
+}
