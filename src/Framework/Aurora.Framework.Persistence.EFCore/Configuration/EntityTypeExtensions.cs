efad721be032293908dcfba36ca5bcfcc5545f78
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-12 10:31:01 -0500

    Aurora.Framework.Persistence.EFCore: add entity type extensions

---
+++ src/Framework/Aurora.Framework.Persistence.EFCore/Configuration/EntityTypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aurora.Framework.Persistence.EFCore.Configuration;
+
+public static class EntityTypeExtensions
+{
+    public static void AddAuditableProperties<T, TId>(
+        this EntityTypeBuilder<T> builder) where T : AuditableEntity<TId>
+    {
+        builder.Property(p => p.CreatedBy).IsRequired().HasMaxLength(100);
+        builder.Property(p => p.CreatedAt).IsRequired();
+        builder.Property(p => p.UpdatedBy).HasMaxLength(100);
+        builder.Property(p => p.UpdatedAt);
+    }
+}
