ba138180a32e75771c50d513f81243441644f422
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-18 15:56:26 -0500

    Aurora.BizSuite.Security: add Application project

---
+++ src/Modules/Security/Aurora.BizSuite.Security.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Aurora.Framework.Behavior;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Aurora.BizSuite.Security.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var executingAssembly = Assembly.GetExecutingAssembly();
+
+        // FluentValidation
+        services.AddValidatorsFromAssembly(executingAssembly);
+
+        // MediatR
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(executingAssembly);
+            config.AddValidationBehavior();
+            config.AddUnitOfWorkBehavior();
+            config.AddPerformanceBehavior();
+            config.AddLoggingBehavior();
+        });
+
+        return services;
+    }
+}
---
+++ src/Modules/Security/Aurora.BizSuite.Security.Application/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Aurora.BizSuite.Security.Domain;
+global using Aurora.BizSuite.Security.Domain.Session;
+global using Aurora.BizSuite.Security.Domain.Users;
+global using Aurora.Framework;
+global using Aurora.Framework.Identity;
+global using FluentValidation;
