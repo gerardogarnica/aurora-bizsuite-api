047bd7d60b20dbfc765c67930f83b349c7c9580e
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-19 12:41:54 -0500

    Aurora.Framework.Application: add rule builder options extensions

---
+++ src/Framework/Aurora.Framework.Application/Extensions/RuleBuilderOptionsExtensions.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Aurora.Framework.Application;
+
+public static class RuleBuilderOptionsExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> WithBaseError<T, TProperty>(
+        this IRuleBuilderOptions<T, TProperty> rule,
+        BaseError baseError) =>
+        rule.WithErrorCode(baseError.Code).WithMessage(baseError.Message);
+}
