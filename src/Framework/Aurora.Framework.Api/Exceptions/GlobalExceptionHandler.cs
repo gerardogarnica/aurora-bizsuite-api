5389fcc81c5194bfe098b69a03040da953d5cd0b
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-20 20:36:30 -0500

    Aurora.Framework.Api: add ValidationException to the global exception handler

--- src/Framework/Aurora.Framework.Api/Exceptions/GlobalExceptionHandler.cs
+++ src/Framework/Aurora.Framework.Api/Exceptions/GlobalExceptionHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Aurora.Framework.Application;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -15,21 +16,48 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IE
     {
         logger.LogError(exception, "Exception ocurred: {Message}", exception.Message);
 
-        var problemDetails = new ProblemDetails
+        string? json;
+
+        if (exception is ValidationException validationException)
         {
-            Type = "Server Error",
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            json = ConvertToValidationProblemDetails(validationException);
+        }
+        else
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            json = ConvertToProblemDetails();
+        }
+
+        httpContext.Response.ContentType = "application/json";
+        await httpContext.Response.WriteAsync(json, cancellationToken);
+
+        return true;
+    }
+
+    private static string ConvertToProblemDetails()
+    {
+        var error = new ProblemDetails
+        {
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
             Title = "Internal Server Error",
             Status = StatusCodes.Status500InternalServerError,
             Detail = "An error occurred while processing your request."
         };
 
-        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        httpContext.Response.ContentType = "application/json";
+        return JsonSerializer.Serialize(error);
+    }
 
-        var json = JsonSerializer.Serialize(problemDetails);
+    private static string ConvertToValidationProblemDetails(ValidationException exception)
+    {
+        var error = new ValidationProblemDetails(exception.Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            Title = exception.Message,
+            Status = StatusCodes.Status400BadRequest,
+            Detail = "See the errors property for details."
+        };
 
-        await httpContext.Response.WriteAsync(json, cancellationToken: cancellationToken);
-
-        return true;
+        return JsonSerializer.Serialize(error);
     }
 }
