b1fb04f5cd7da14c651273688507997ce829e4cc
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-22 21:28:20 -0500

    Aurora.Framework.Application: add IQuery interfaces

---
+++ src/Framework/Aurora.Framework.Application/Messaging/IQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Aurora.Framework.Application;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>;
---
+++ src/Framework/Aurora.Framework.Application/Messaging/IQueryHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Aurora.Framework.Application;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>;
