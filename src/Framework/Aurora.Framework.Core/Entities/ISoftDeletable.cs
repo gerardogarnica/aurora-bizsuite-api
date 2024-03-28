b6948fe29a274c8c78e2734027f90b62befb0e08
gerardogarnica <gerardo.garnica@gmail.com>
2024-03-28 14:22:02 -0500

    Aurora.Framework.Core: add soft deletable interface

---
+++ src/Framework/Aurora.Framework.Core/Entities/ISoftDeletable.cs
@@ -0,0 +1,8 @@
+﻿namespace Aurora.Framework;
+
+public interface ISoftDeletable
+{
+    bool IsDeleted { get; set; }
+    public string? DeletedBy { get; set; }
+    DateTime? DeletedAt { get; set; }
+}
