beb5adbcdbe9a4ec0bb882486b672df1ad20f232
gerardogarnica <gerardo.garnica@gmail.com>
2024-05-16 16:46:32 -0500

    Aurora.Framework.Application: change unit of work behavior

--- src/Framework/Aurora.Framework.Application/Behaviors/UnitOfWorkBehavior.cs
+++ src/Framework/Aurora.Framework.Application/Behaviors/UnitOfWorkBehavior.cs
@@ -6,15 +6,14 @@ namespace Aurora.Framework.Application;
 internal sealed class UnitOfWorkBehavior<TRequest, TResponse>(
     IUnitOfWork unitOfWork)
     : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : IRequest<TResponse>
+    where TRequest : IBaseCommand
+    where TResponse : Result
 {
     public async Task<TResponse> Handle(
         TRequest request,
         RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
-        if (IsNotCommand()) return await next();
-
         using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
 
         var response = await next();
@@ -22,11 +21,5 @@ internal sealed class UnitOfWorkBehavior<TRequest, TResponse>(
         transactionScope.Complete();
 
         return response;
-    }
-
-    private static bool IsNotCommand()
-    {
-        return typeof(TRequest).GetInterface("ICommand") is null
-            && typeof(TRequest).GetInterface("ICommand`1") is null;
     }
 }
