88d33250c6193e0a525f32d0b841100bf031e081
gerardogarnica <gerardo.garnica@gmail.com>
2024-06-06 15:44:34 -0500

    Aurora.BizSuite.Items: add repository methods

--- src/Modules/Items/Aurora.BizSuite.Items.Domain/Items/IItemRepository.cs
+++ src/Modules/Items/Aurora.BizSuite.Items.Domain/Items/IItemRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Aurora.BizSuite.Items.Domain.Items;
+﻿using Aurora.BizSuite.Items.Domain.Categories;
+
+namespace Aurora.BizSuite.Items.Domain.Items;
 
 public interface IItemRepository : IRepository<Item>
 {
     Task<Item?> GetByIdAsync(ItemId id);
+    Task<PagedResult<Item>> GetPagedAsync(PagedViewRequest paged, CategoryId? categoryId, ItemType? type, ItemStatus? status, string? searchTerms);
+    Task InsertAsync(Item item);
+    void Update(Item item);
 }
--- src/Modules/Items/Aurora.BizSuite.Items.Infrastructure/Items/ItemRepository.cs
+++ src/Modules/Items/Aurora.BizSuite.Items.Infrastructure/Items/ItemRepository.cs
@@ -10,6 +10,45 @@ internal sealed class ItemRepository(
         .Items
         .Include(x => x.Category)
         .Include(x => x.MainUnit)
+        .Include(x => x.Units)
         .Where(x => x.Id == id)
         .FirstOrDefaultAsync();
+
+    public async Task<PagedResult<Item>> GetPagedAsync(
+        PagedViewRequest paged,
+        CategoryId? categoryId,
+        ItemType? type,
+        ItemStatus? status,
+        string? searchTerms)
+    {
+        var query = dbContext
+            .Items
+            .AsNoTracking()
+            .AsQueryable();
+
+        if (categoryId is not null)
+        {
+            query = query.Where(x => x.CategoryId == categoryId);
+        }
+
+        if (type is not null)
+        {
+            query = query.Where(x => x.ItemType == type);
+        }
+
+        if (status is not null)
+        {
+            query = query.Where(x => x.Status == status);
+        }
+
+        if (!string.IsNullOrWhiteSpace(searchTerms) && searchTerms.Length >= 3)
+        {
+            query = query.Where(x =>
+                x.Code.Contains(searchTerms) ||
+                x.Name.Contains(searchTerms) ||
+                x.Description.Contains(searchTerms));
+        }
+
+        return await ToPagedResultAsync(query.OrderBy(x => x.Name), paged);
+    }
 }
