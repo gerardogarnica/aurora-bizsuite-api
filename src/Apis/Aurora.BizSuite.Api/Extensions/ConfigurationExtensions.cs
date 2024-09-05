a6e5cbcb14faf860d805eaf3cb29623fc3c6742d
gerardogarnica <gerardo.garnica@gmail.com>
2024-09-05 12:41:24 -0500

    Aurora.BizSuite.Api: add configuration extensions

---
+++ src/Apis/Aurora.BizSuite.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Aurora.BizSuite.Api.Extensions;
+
+internal static class ConfigurationExtensions
+{
+    internal static void AddModuleConfiguration(this IConfigurationBuilder builder, string[] moduleNames)
+    {
+        foreach (var module in moduleNames)
+        {
+            builder.AddJsonFile($"modules.{module}.json", false, true);
+            builder.AddJsonFile($"modules.{module}.Development.json", true, true);
+        }
+    }
+}
