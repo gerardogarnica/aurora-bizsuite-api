585f90787fabae8476d10e4c4508799cfb7a8f6e
gerardogarnica <gerardo.garnica@gmail.com>
2024-09-05 21:27:08 -0500

    Aurora.BizSuite.Companies: add scaffolding classes

---
+++ src/Modules/Companies/Aurora.BizSuite.Companies.Presentation/EndpointTags.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurora.BizSuite.Companies.Presentation;
+
+internal static class EndpointTags
+{
+    internal const string Company = "Company";
+    internal const string Store = "Store";
+}
