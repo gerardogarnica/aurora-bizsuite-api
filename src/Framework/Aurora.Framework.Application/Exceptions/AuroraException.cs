3752a9a2dffcef90147f0d4577c9b84f705deff9
gerardogarnica <gerardo.garnica@gmail.com>
2024-09-13 13:35:33 -0500

    Aurora.Framework.Application: add Aurora generic exception class

---
+++ src/Framework/Aurora.Framework.Application/Exceptions/AuroraException.cs
@@ -0,0 +1,8 @@
+﻿namespace Aurora.Framework.Application;
+
+public class AuroraException(string requestName, BaseError? error, Exception? innerException = default)
+    : Exception("Aurora BizSuite exception", innerException)
+{
+    public string RequestName { get; } = requestName;
+    public BaseError? Error { get; } = error;
+}
