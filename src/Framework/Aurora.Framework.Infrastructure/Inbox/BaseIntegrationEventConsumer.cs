8f1f53eee17ca6b01d0ab2a7049424554678aa1b
gerardogarnica <gerardo.garnica@gmail.com>
2024-09-22 21:51:24 -0500

    Aurora.Framework.Infrastructure: add integration event consumer base class

---
+++ src/Framework/Aurora.Framework.Infrastructure/Inbox/BaseIntegrationEventConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Aurora.Framework.Infrastructure.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Aurora.Framework.Infrastructure.Inbox;
+
+public abstract class BaseIntegrationEventConsumer(DbContext dbContext)
+{
+    public async Task ProcessIntegrationEventMessage(IIntegrationEvent message)
+    {
+        var inboxMessage = new InboxMessage
+        {
+            Id = message.Id,
+            Type = message.GetType().Name,
+            Content = JsonConvert.SerializeObject(message, SerializerSettings.Instance),
+            OccurredOnUtc = message.OccurredOnUtc,
+            IsProcessed = false
+        };
+
+        await dbContext.Set<InboxMessage>().AddRangeAsync(inboxMessage);
+
+        await dbContext.SaveChangesAsync();
+    }
+}
